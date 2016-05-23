f80c977b8ab504f8aef442cdbca65c85a73f2989
Nicolas Agustin Torers <nicolastrres@gmail.com>
2016-05-22 23:13:06 -0300

    Creating basic project structure

--- webcaesar.py
+++ webcaesar.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pycaesar
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Webcaesar'
+    return render_template('index.html')
+
+
+@app.route('/encrypt', methods=['GET', 'POST'])
+def encrypt():
+    if request.method == 'POST':
+        # TODO: sanitize input
+        cipher_text = pycaesar.encrypt(request.form.get('plain_text'), request.form.get('key'))
+        return render_template('index.html', cipher_text=cipher_text)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
