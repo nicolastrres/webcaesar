d2bbe34562128b9f7afd28f69ff63d333ae47e7f
Nicolas Agustin Torers <nicolastrres@gmail.com>
2016-05-24 22:25:08 -0300

    Refactoring tests structure

--- tests.py
+++ tests.py
@@ -9,10 +9,12 @@ class WebCaesarTest(unittest.TestCase):
         self.app = webcaesar.app.test_client()
 
     def test_encrypt(self):
-        response = self.app.post('/encrypt', data=dict(plain_text='agustin', key=1), follow_redirects=True)
+        response = self.app.post(
+            '/encrypt',
+            data=dict(plain_text='agustin', key=1),
+            follow_redirects=True
+        )
         assert 'bhvtujo' in str(response.data)
 
 if __name__ == '__main__':
     unittest.main()
-
-
