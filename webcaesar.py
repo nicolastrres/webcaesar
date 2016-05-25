4dbdc6d01fb2951a26be05ef1c872ee2cdcf8cf2
Nicolas Agustin Torers <nicolastrres@gmail.com>
2016-05-24 22:35:24 -0300

    Adding procfile, runtime in order to deploy to heroku

--- webcaesar.py
+++ webcaesar.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 import pycaesar
 
@@ -19,4 +21,5 @@ def encrypt():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
